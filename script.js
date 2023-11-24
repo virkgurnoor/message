049d584971cef15e7e9f4f9f0e791c48cebc4edd
virkgurnoor <151964930+virkgurnoor@users.noreply.github.com>
2023-11-24 15:09:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,9 @@ const gif = document.querySelector(".gif");
 const noBtn = document.querySelector(".no-btn");
 const yesBtn = document.querySelector(".yes-btn");
 
-yesBtn.addEventListener("mouseover", () => {
-  const noBtnRect = yesBtn.getBoundingClientRect();
-  const maxX = window.innerWidth - noBtnRect.width;
-  const maxY = window.innerHeight - noBtnRect.height;
-
-  const randomX = Math.floor(Math.random() * maxX);
-  const randomY = Math.floor(Math.random() * maxY);
-
-  yesBtn.style.left = randomX + "px";
-  yesBtn.style.top = randomY + "px";
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "I am sorry ";
+  gif.src =
+    "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
+
